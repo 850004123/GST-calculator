e9b6dfff7461a545ead044651140fb15945fdf99
850004123 <850004123@nzse.ac.nz>
2025-03-22 11:16:12 +1300

    update code

--- GST calculator/Program.cs
+++ GST calculator/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace GST_calculator
+{
+    class Program;
+    {
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the amount: ");
+        double amount = Convert.ToDouble(Console.ReadLine());
+        double gst = amount * 0.18;
+        Console.WriteLine("GST amount is: " + gst);
+    }
+}
