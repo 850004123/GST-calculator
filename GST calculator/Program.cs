377889b64acb11a07e1d075fc3706ba179c25c52
850004123 <850004123@nzse.ac.nz>
2025-03-22 11:26:07 +1300

    update

--- GST calculator/Program.cs
+++ GST calculator/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 
-namespace GST_calculator
+namespace GST_Calculator
 {
-    class Program;
+    class Program
+
     {
         static void Main(string[] args)
-    {
-        Console.WriteLine("Enter the amount: ");
-        double amount = Convert.ToDouble(Console.ReadLine());
-        double gst = amount * 0.18;
-        Console.WriteLine("GST amount is: " + gst);
+
+            // Written by Jordan.E
+
+
+        
+        {
+            // Asks user to enter product name
+            Console.WriteLine("Enter the product name: ");
+
+            // Asks the user to enter the amount
+            Console.WriteLine("Enter the amount: ");
+
+            // Converts the entered amount to double
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            // Calculates the GST amount
+            double gst = amount * 0.18;
+
+            // Displays the GST amount
+            Console.WriteLine("GST amount is: " + gst);
+        }
     }
 }
